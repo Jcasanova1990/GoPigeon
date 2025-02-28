e91f2109e722bfaf286ba37c093f0492724a6a19
Jcasanova1990 <jcasanova1990@gmail.com>
2025-02-27 21:32:24 -0500

    patch 4.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from 'react';
-import './Global.scss';
 import { useMediaQuery } from 'react-responsive';
 import ThreeCanvas from './components/ThreeCanvas';
 import ReactDOM from 'react-dom/client';
@@ -14,16 +13,53 @@ function App() {
     }
   }, []);
 
+  const styles = {
+    global: {
+      margin: 0,
+      padding: 0,
+      boxSizing: 'border-box',
+      width: '100%',
+      height: '100%',
+      overflow: 'hidden',
+    },
+    app: {
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#000', // Black background
+      color: 'white',
+      fontFamily: 'Arial, sans-serif',
+    },
+    mobileMessage: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      textAlign: 'center',
+      padding: '20px',
+      fontSize: '18px',
+      backgroundColor: '#000', // Black background
+      color: 'red', // Red text
+    },
+  };
+
+  useEffect(() => {
+    Object.assign(document.body.style, styles.global);
+    Object.assign(document.documentElement.style, styles.global);
+  }, []);
+
   if (isMobile) {
     return (
-      <div className="MobileMessage">
+      <div style={styles.mobileMessage}>
         <p>Sorry, this site is currently not available on mobile devices at this time. Please visit on a tablet, laptop, or desktop.</p>
       </div>
     );
   }
 
   return (
-    <div className="App">
+    <div style={styles.app}>
       <ThreeCanvas ref={gameRef} />
     </div>
   );
